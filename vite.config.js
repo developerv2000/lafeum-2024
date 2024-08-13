f86c450abb0c669e71858ec93667254f7aac3cd7
MrBobPRO <bobnuridinov@gmal.com>
2024-08-13 15:32:30 +0500

    Installed Laravelx11 & preapared assets folder

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/front/main.css',
+                'resources/css/dashboard/dashboard.css',
+                'resources/js/front/main.js',
+                'resources/js/dashboard/dashboard.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    publicDir: 'public',
+    base: '/',
+    build: {
+        assetsDir: '',
+        copyPublicDir: false,
+    },
+});
+
